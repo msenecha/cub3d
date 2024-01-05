6727fc1927cc473669acaa7eb6e766d28b526023
msenecha <marinesenechal96@gmail.com>
2024-01-05 16:25:19 +0100

    version finale ok

--- sources/errors_msg.c
+++ sources/errors_msg.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   errors_msg.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svidal <svidal@student.42.fr>              +#+  +:+       +#+        */
+/*   By: msenecha <msenecha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 16:35:36 by svidal            #+#    #+#             */
-/*   Updated: 2024/01/04 16:35:38 by svidal           ###   ########.fr       */
+/*   Updated: 2024/01/05 16:22:14 by msenecha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,8 @@
 
 int	error_msg(char *detail, char *str, int code)
 {
-	ft_putstr_fd(RED "cub3D: Error", 2);
 	if (detail)
-	{
-		ft_putstr_fd(": ", 2);
 		ft_putstr_fd(detail, 2);
-	}
 	if (str)
 	{
 		ft_putstr_fd(": ", 2);
@@ -31,9 +27,7 @@ int	error_msg(char *detail, char *str, int code)
 
 int	error_msg_val(int detail, char *str, int code)
 {
-	ft_putstr_fd(RED "cub3D: Error: ", 2);
-	ft_putnbr_fd(detail, 2);
-	ft_putstr_fd(": ", 2);
+	(void)detail;
 	ft_putstr_fd(str, 2);
 	ft_putstr_fd("\n" RESET, 2);
 	return (code);
